c52fe88ebe27a2f5ed2be56a6e45a285a24a3b63
Moloko Sehata <moloko4@live.com>
2023-10-10 11:35:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Constructors
+{
+    public class book
+    {
+        public string tittle;
+        public string author;
+        public int pages;
+
+        public book(string aTittle, string anAuthor, int aPages)
+        {
+            tittle = aTittle;
+            author = anAuthor;
+            pages = aPages;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book("Its More Than Just Money", "Witness Mdaka", 400);
+
+            Console.WriteLine("Book 1 Author: " + book1.author);
+
+            Console.Read();
+        }
+        
+    }
+}
